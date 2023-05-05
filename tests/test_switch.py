ae52931c746b5d0b2d91a150b6140ad100e82a91
Matt Rayner <mattrayner@users.noreply.github.com>
2023-05-05 16:21:25 +0100

    Add a switch for charge mode (#31)
    * [#30] Add a switch to toggle between smart and manual charge modes
    
    * Update example YML in README and keep smart mode switch on during override
    
    * Update example image to include charge mode information
    
    * Update README and INFO to include updated YML to match example image
    
    * Rev version
    
    * Fix test error

--- tests/test_switch.py
+++ tests/test_switch.py
@@ -1,28 +1,22 @@
 """Test pod_point switch."""
-import asyncio
-import pytest
+from unittest.mock import patch
 
-import aiohttp
-import homeassistant.helpers.aiohttp_client as client
-from .fixtures import POD_COMPLETE_FIXTURE
-from unittest.mock import call, patch
-
-from homeassistant.components import switch
 from homeassistant.components.switch import SERVICE_TURN_OFF, SERVICE_TURN_ON
 from homeassistant.const import ATTR_ENTITY_ID
+from podpointclient.pod import Pod
+import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.pod_point import async_setup_entry
-from custom_components.pod_point.const import DEFAULT_NAME, DOMAIN, SWITCH
-
-from podpointclient.pod import Pod
+from custom_components.pod_point.const import DOMAIN, SWITCH
 
 from .const import MOCK_CONFIG
 
+
 @pytest.mark.asyncio
 @pytest.mark.enable_socket
-async def test_switch_services(hass, bypass_get_data):
-    """Test switch services."""
+async def test_allow_charging_switch(hass, bypass_get_data):
+    """Test allow charging switch"""
     # Create a mock entry so we don't have to go through config flow
     config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
     assert await async_setup_entry(hass, config_entry)
@@ -57,4 +51,39 @@ async def test_switch_services(hass, bypass_get_data):
         flag = title_func.call_args.kwargs["enabled"]
         pod_type = type(title_func.call_args.kwargs["pod"])
         assert False == flag
+        assert Pod == pod_type
+
+@pytest.mark.asyncio
+@pytest.mark.enable_socket
+async def test_charge_mode_switch(hass, bypass_get_data):
+    """Test charge mode switch"""
+    # Create a mock entry so we don't have to go through config flow
+    config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
+    assert await async_setup_entry(hass, config_entry)
+    await hass.async_block_till_done()
+
+    # Functions/objects can be patched directly in test code as well and can be used to test
+    # additional things, like whether a function was called or what arguments it was called with
+    with patch("podpointclient.client.PodPointClient.async_set_charge_mode_smart") as smart_mode_func:
+        await hass.services.async_call(
+            SWITCH,
+            SERVICE_TURN_ON,
+            service_data={ATTR_ENTITY_ID: "switch.psl_123456_smart_charge_mode"},
+            blocking=True,
+        )
+        assert smart_mode_func.called
+
+        pod_type = type(smart_mode_func.call_args.args[0])
+        assert Pod == pod_type
+
+    with patch("podpointclient.client.PodPointClient.async_set_charge_mode_manual") as manual_mode_func:
+        await hass.services.async_call(
+            SWITCH,
+            SERVICE_TURN_OFF,
+            service_data={ATTR_ENTITY_ID: "switch.psl_123456_smart_charge_mode"},
+            blocking=True,
+        )
+        assert manual_mode_func.called
+
+        pod_type = type(manual_mode_func.call_args.args[0])
         assert Pod == pod_type
