4f732bffb44d53b3b53bf9c9a2891f92949d4e18
munaaf <munaafg2004@hotmail.com>
2024-09-16 17:04:02 +0100

    Update entity.py
    Fixing bad if statement :D to in check

--- custom_components/pod_point/entity.py
+++ custom_components/pod_point/entity.py
@@ -355,7 +355,7 @@ class PodPointEntity(CoordinatorEntity):
 
         img = model_type
 
-        if model_id == "03" || model_id == "05":
+        if model_id in ("03", "05"):
             img = f"{model_type}-{model_id}"
 
         return f"{APP_IMAGE_URL_BASE}/{img.lower()}.png"
